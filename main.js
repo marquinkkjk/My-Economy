a8cfb3ced8fb383052e9c9a78785f394d939ec1f
marquinkkjk <marcosdiasbarbosa24@gmail.com>
2025-02-13 20:16:55 -0300

    dashboardUpdate

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let valorTotal = document.getElementById('valor')
-let totalEdit = document.getElementById('editValue')
-let addExpense = document.getElementById('addExpense')
+const totalEdit = document.getElementById('editValue')
+const addExpense = document.getElementById('addExpense')
 let input = document.querySelector('.input')
 let expenseValue = document.querySelector('.value')
 let calculate = 0
+let typeSave
+const valueSave = [0, 0, 0, 0, 0, 0, 0]
 const dashview = document.getElementById('dashview')
-const dashboard = document.querySelector('#dashboard')
+const dashboard = document.getElementById('dashboard')
+let expenses = document.getElementById('expenselist')
 const buttonClose = document.getElementById('close')
 
 if(valorTotal.value === "") { 
@@ -24,16 +27,17 @@ addExpense.addEventListener('click', (event) => {
     event.preventDefault()
 })
 
-expenseChange()
+totalChange()
 
-function expenseChange() {
+function totalChange() {
     addExpense.addEventListener('click', () => {
        if (input.value !== "") {
-        datasave = input.value
+        typeSave = input.value
+        expenseChange()
         calculate = parseFloat(valorTotal.value) - parseFloat(expenseValue.value)
         valorTotal.value = calculate
         input.value = ''
-        expenseValue.value = '' 
+        expenseValue.value = ''
        } 
        if(isNaN(valorTotal.value)) {
         alert("PREENCHA APENAS COM NÚMEROS!")
@@ -42,9 +46,9 @@ function expenseChange() {
        })
 }
 
-dataUpdate()
-
 openDash ()
+closeDash ()
+
 
 function openDash() {
     dashview.onclick = () => {
@@ -52,7 +56,93 @@ function openDash() {
     }
 }
 
-closeDash ()
+function expenseChange() {
+  switch (typeSave) {
+   case 'Contas':
+    valueSave[0] += parseInt(expenseValue.value)
+    expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Mercado':
+        valueSave[1] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Uber':
+        valueSave[2] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Lanche':
+        valueSave[3] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Presente':
+        valueSave[4] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Assinaturas':
+        valueSave[5] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+
+    case 'Outros':
+        valueSave[6] += parseInt(expenseValue.value)
+        expenses.innerHTML = `
+        <li>Contas - $${valueSave[0]},00</li>
+        <li>Mercado - $${valueSave[1]},00</li>
+        <li>Uber - $${valueSave[2]},00</li>
+        <li>Lanche - $${valueSave[3]},00</li>
+        <li>Presente - $${valueSave[4]},00</li>
+        <li>Assinaturas - $${valueSave[5]},00</li>
+        <li>Outros - $${valueSave[6]},00</li>`
+    break
+  }
+}
 
 function closeDash() {
     buttonClose.onclick = () => {
